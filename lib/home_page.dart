76d58f68ea3e1195bf738646f711714de8f93d08
fortuna <avidia38@gmail.com>
2022-10-19 11:55:51 +0900

    feat: dispalayExOh and Gesturedector

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  String displayExOh = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,14 +19,30 @@ class _HomePageState extends State<HomePage> {
           itemCount: 9,
             gridDelegate:
                 const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+
             itemBuilder: (context, index) {
-              return Center(
-                child: Text(index.toString(),
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 40,
-                    ),),
+
+              return GestureDetector(
+                onTap: _tapped,
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey.shade700),
+                  ),
+                  child: Center(
+                    child: Text(displayExOh,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                        ),),
+                  ),
+                ),
               );
-            }));
+            },),);
+  }
+
+  void _tapped() {
+    setState(() {
+      displayExOh = 'o';
+    });
   }
 }
